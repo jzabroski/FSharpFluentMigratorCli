ed8f8edcf41030bd90570cd33b011ad8e4d5a9a9
Voronin Pavel <voroninp@gmail.com>
2020-03-27 14:58:06 +0100

    Add project files.

---
+++ DbMigrations/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace DbMigrations
+
+open FluentMigrator
+open System
+    
+[<Migration(1L)>]
+type AddTable() = 
+    inherit Migration()
+
+    override this.Up() = 
+        Console.WriteLine(System.AppContext.GetData("APP_CONTEXT_DEPS_FILES"))
+        this.Create.Table("Table")
+            .WithColumn("Column").AsString(10, "Latin1_General_CI_AS").NotNullable().Indexed().PrimaryKey() |> ignore
+        ()
+
+    override this.Down() =
+        this.Delete.Table("Table") |> ignore
+        ()
