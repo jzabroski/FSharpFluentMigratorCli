f15820378f8ae382d1cb1ffad8dee9496446874d
John Zabroski <johnzabroski@gmail.com>
2020-04-04 19:03:40 -0400

    Demo of https://github.com/fluentmigrator/fluentmigrator/issues/883

--- ConsoleFSharpApp/Program.fs
+++ ConsoleFSharpApp/Program.fs
@@ -1,8 +1,45 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging;
+open FluentMigrator
+open FluentMigrator.Runner
+
+
+type DatabaseConfiguration(processorId : string, connectionString : string) =
+     member val ProcessorId = processorId with get, set
+     member val ConnectionString = connectionString with get, set
+
+let ConfigureApp (databaseConfiguration : DatabaseConfiguration -> unit) =
+    ignore
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let dbConfig = DatabaseConfiguration("SqlServer2017", "Data Source=.;Initial Catalog=DATABASE_NAME;Integrated Security=True;")
+
+
+    let loggingServices = new Action<ILoggingBuilder>(fun (lb : ILoggingBuilder) -> lb.AddDebug().AddFluentMigratorConsole() |> ignore)
+    let runnerConfig = new Action<IMigrationRunnerBuilder>(fun (builder : IMigrationRunnerBuilder) ->
+        builder
+            .AddSqlServer()
+            .WithGlobalConnectionString(dbConfig.ConnectionString)
+            .ScanIn(typeof<DbMigrations.AddTable>.Assembly).For.Migrations() |> ignore)
+
+    let serviceCollection = new ServiceCollection()
+    serviceCollection
+        .AddLogging(loggingServices)
+        .AddFluentMigratorCore()
+        .ConfigureRunner(
+            runnerConfig
+        )
+        |> ignore
+
+    let serviceProvider = serviceCollection.BuildServiceProvider()
+
+    let runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+    runner.MigrateUp()
+    printfn "We did it!!"
     0 // return an integer exit code
